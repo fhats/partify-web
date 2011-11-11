f1b25f7b185cff83555e54fe9e33da9a700aefae
Fred Hatfull <fred.hatfull@gmail.com>
2011-11-11 17:26:29 -0500

    Redirect to github still

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import os
-from flask import Flask
+from flask import Flask, redirect
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Python!"
+    return redirect("https://github.com/fxh32/partify")
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 80))
