73e6f7c17d2f19d14c4d75ad1b2cf513bf3b564b
Fred Hatfull <fred.hatfull@gmail.com>
2011-11-26 21:02:19 -0500

    Redirect to wiki instead of main page.

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return redirect("https://github.com/fxh32/partify")
+    return redirect("https://github.com/fxh32/partify/wiki")
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 80))
