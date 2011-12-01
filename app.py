3bcf6915fd084d6df536a2d3c890eec476c29910
Stephen Johnson <srj15@case.edu>
2011-11-30 19:56:26 -0500

    Added a URL handler for installation page (fix #1)

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def hello():
     return redirect("https://github.com/fxh32/partify/wiki")
 
+@app.route("/install")
+def install():
+    return redirect("https://github.com/fxh32/partify/wiki/Installation")
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 80))
     app.run(host='0.0.0.0', port=port)
