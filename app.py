418dab6e46fbcce6dd7e93f3c0545ff8f9201dbc
Stephen Johnson <srj15@case.edu>
2011-11-30 20:01:06 -0500

    Abstracted base URL

--- app.py
+++ app.py
@@ -2,13 +2,15 @@ import os
 from flask import Flask, redirect
 app = Flask(__name__)
 
+base_url = "https://github.com/fxh32/partify/"
+
 @app.route("/")
 def hello():
-    return redirect("https://github.com/fxh32/partify/wiki")
+    return redirect(base_url + "wiki")
 
 @app.route("/install")
 def install():
-    return redirect("https://github.com/fxh32/partify/wiki/Installation")
+    return redirect(base_url + "wiki/Installation")
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 80))
