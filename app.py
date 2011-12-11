439386ab23fb9113ab164bd590ea6a40b3cd9308
Fred Hatfull <fred.hatfull@gmail.com>
2011-12-11 04:01:52 -0500

    .

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import tornado.ioloop
 import tornado.web
 
 if __name__ == "__main__":
+    # Does the stuff
     application = tornado.web.Application([
         (r"/docs/(.*)", tornado.web.StaticFileHandler, {"path": "docs"}),
 	(r"/", tornado.web.RedirectHandler, {"url": "https://github.com/fxh32/partify/wiki"}),
