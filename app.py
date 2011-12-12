65a438201d01c0ba49f2835d838f28acfb03100e
Fred Hatfull <fred.hatfull@gmail.com>
2011-12-12 06:13:54 -0500

    Re-serve static files

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ if __name__ == "__main__":
     # Does the stuff
     application = tornado.web.Application([
         (r"/docs/(.*)", tornado.web.StaticFileHandler, {"path": "docs"}),
+	(r"/static/(.*)", tornado.web.StaticFileHandler, {"path": "static"}),
 	(r"/", tornado.web.RedirectHandler, {"url": "https://github.com/fxh32/partify/wiki"}),
 	(r"/install", tornado.web.RedirectHandler, {"url": "https://github.com/fxh32/partify/wiki/Installation"}),
     ])
